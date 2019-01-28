e4e7aa888e18d1e5e2114bf481c56792c5544d4e
Rebecca Jackson <zimbecs@gmail.com>
2019-01-28 19:46:08 +1300

    updated join table with correct data

--- migrations/20190126155148_veg.js
+++ migrations/20190126155148_veg.js
@@ -3,10 +3,9 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('veg', (table) => {
     table.increments('id')
     table.string('name')
-    table.integer('plant_id').references('plant.id')
     table.string('type')
     table.string('harvest')
-    table.string('condition')
+    table.string('spaceplants')
     table.string('pest')
     table.string('tips')
     table.string('img_url').defaultTo('/veg.png')
